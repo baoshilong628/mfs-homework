1f6e9ab437e1657a00de1b011e3bf7535a3e21ba
鲍世龙 <1282936365@qq.com>
2021-11-23 00:50:33 +0800

    work

---
+++ 单文件组件/my-counter/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    {{value}}
+    <Counter @inc="inc"></Counter>
+    <Counter @inc="inc"></Counter>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter";
+export default {
+  data:function (){
+    return{
+      value:0
+    }
+  },
+  name: 'App',
+  methods:{
+    inc(){
+      this.value++
+    }
+  },
+  components:{
+    Counter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 单文件组件/my-counter/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="inc">
+    {{ value }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data: function () {
+    return {
+      value:0
+    }
+  },
+  methods:{
+    inc(){
+      this.value++
+      this.$emit("inc")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
